cd39b235a11adcfbbc45ce08bcc347172b394dd1
Ryan Ecarma <rpecarma@gmail.com>
2021-06-18 05:12:16 -0500

    implemented category create read delete, status messages for category create

--- frontend/actions/category.js
+++ frontend/actions/category.js
@@ -4,6 +4,7 @@ import fetch from 'isomorphic-fetch';
 //bring in API from our config
 import {API} from '../config';
 
+//refer to backend/routes/category.js
 export const create = (category, token) => {
     console.log(`token ${token}`)
     console.log(category)
@@ -15,6 +16,44 @@ export const create = (category, token) => {
             Authorization: `bearer ${token}`
         },
         body: JSON.stringify(category)
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: 'GET',
+        
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+export const singleCategory = (slug) => { 
+    return fetch(`${API}/category/${slug}`, {
+        method: 'GET',
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+export const removeCategory = (slug, token) => {
+    //console.log(`token ${token}`)
+    //console.log(slug)
+    return fetch(`${API}/category/${slug}`, {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `bearer ${token}`
+        },
     })
     .then(response => {
         return response.json()
--- frontend/components/crud/Category.js
+++ frontend/components/crud/Category.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 import { isAuth, getCookie } from '../../actions/auth'; //get token from isAuth.  Get gookie from local storage and send it when create is called from actions
-import { create } from '../../actions/category';
+import { create, getCategories, removeCategory } from '../../actions/category';
 
 const Category = () => {
     //we will have alot of categories when we request from backend so they will be stored in the categories array
@@ -11,12 +11,61 @@ const Category = () => {
         error: false,
         success: false,
         categories: [],
-        removed: false
+        removed: false,
+        reload: false
     })
+
+    //destructure from values
+    const { name, error, success, categories, removed, reload } = values;
+    const token = getCookie('token');
+
+    //load the categories when the component mounts
+    useEffect(() =>{
+        loadCategories()
+    }, [reload])
+
+    const loadCategories = () => {
+        getCategories().then(data => {
+            if(data.error) {
+                console.log(data.error)
+            } else {
+                setValues({...values, categories: data})
+            }
+        })
+    };
+
+    const showCategories = () => {
+        return categories.map((category, index) => {
+            console.table(category);
+            return <button onDoubleClick={() => deleteConfirm(category.slug)} 
+            title="Double click to delete" key={index} 
+            className="btn btn-outline-primary mr-1 ml-1 mt-3">{category.name}</button>
+        })
+    }
+
+    const deleteConfirm = slug => {
+        let answer = window.confirm('Are you sure you want to delete this category?');
+        if(answer) {
+            deleteCategory(slug)
+        }
+    }
+
+    const deleteCategory = slug => {
+        //console.log('delete', slug);
+        removeCategory(slug, token).then(data => {
+            if(data.error) {
+                console.log(data.error)
+            } else {
+                //console.log('check error', data.error);
+                setValues({...values, error: false, success: false, name: '', removed: !removed, reload: !reload});
+            }
+        })
+    }
 
     const clickSubmit = (e) => {
         e.preventDefault();
         //console.log('create category', name);
+        // {name} == {name: name}
         create({name},token).then(data => {
             if(data.error){
                 setValues({
@@ -29,7 +78,9 @@ const Category = () => {
                     ...values,
                     error: false,
                     success: true,
-                    name: ''
+                    name: '',
+                    removed: '',
+                    reload: !reload
                 })
             }
         })
@@ -39,9 +90,27 @@ const Category = () => {
         setValues({ ...values, name: e.target.value, error: false, success: false, removed: '' })
     }
 
-    //destructure from values
-    const { name, error, success, categories, removed } = values;
-    const token = getCookie('token');
+    const showSuccess = () => {
+        if(success) {
+            return <p className="text-success">Category successfully created.</p>
+        }
+    }
+
+    const showError = () => {
+        if(error) {
+            return <p className="text-danger">Category already exists.</p>
+        }
+    }
+
+    const showRemoved = () => {
+        if(removed) {
+            return <p className="text-danger">Category removed.</p>
+        }
+    }
+
+    const mouseMoveHanlder = e => {
+        setValues({...values, error: false, success: false, removed: ''})
+    }
 
     const newCategoryForm = () => {
 
@@ -60,7 +129,14 @@ const Category = () => {
     }
 
     return (<React.Fragment>
+        {showSuccess()}
+        {showError()}
+        {showRemoved()}
+        
+        <div onMouseMove={mouseMoveHanlder}>
         {newCategoryForm()}
+        {showCategories()}
+        </div>
     </React.Fragment>
     )
 
