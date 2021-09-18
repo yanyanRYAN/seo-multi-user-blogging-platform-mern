897da673cba9d1d2fcdb2bedf0d291e5ae3a392d
Ryan Ecarma <rpecarma@gmail.com>
2021-09-18 04:16:41 -0500

    working on index landing page. added some images

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,24 @@
 import Layout from '../components/Layout'
 import Link from 'next/link'
 import { APP_NAME } from '../config';
+import { urlObjectKeys } from 'next/dist/next-server/lib/utils';
 
+const styles = {
+    
+    "background-position" : "center",
+    "height" : "15vw",
+    "width" : "100%",
+    "object-fit" : "cover",
+    "opacity" : '50%'
+}
 
 const Index = () => {
 
-    
-    
+
+
     return (
         <Layout hideSearch="false">
-        
+
             <article className="overflow-hidden">
                 {/* <div>
                     <section>
@@ -18,19 +27,51 @@ const Index = () => {
                 </div> */}
                 <div className="jumbotron jumbotron-fluid home-jumbotron-image">
                     <div className="container">
-                        <h1 className="display-4 text-light font-weight-bold">{APP_NAME}</h1>
+                        <h1 className="display-4 text-light font-weight-bold mt-4">{APP_NAME}</h1>
                         <p className="lead text-light">Random blogs from random dweebs</p>
                         <hr className="my-4"></hr>
-                        <p className="lead">
+                        {/* <p className="lead">
                             <a className="btn btn-outline-light btn-lg" href="/blogs" role="button">Check 'Em</a>
-                        </p>
+                        </p> */}
                     </div>
                 </div>
 
 
                 <div className="container">
-                    <div className="text-center">
-                        
+                    <div className="row justify-content-between">
+                        {/* <div className="col-md-8 offset-4" style={{backgroundColor:'grey', backgroundImage: "url(" + "/static/images/nakamagarage.jpg" + ")"}}>
+                                sup
+                            </div> */}
+                        <div className="col-md-5 mt-4">
+                            <div className="card bg-dark text-white">
+                                <img className="card-img" style={styles} src="/static/images/front-page/rimukoro.png" alt="Card image" />
+                                <div className="card-img-overlay">
+                                    <h5 className="card-title">Signin</h5>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-5 mt-4">
+                            <div className="card bg-dark text-white">
+                                <img className="card-img" style={styles} src="/static/images/front-page/dongho-kang.jpg" alt="Card image" />
+                                <div className="card-img-overlay">
+                                    <h5 className="card-title">Read Blogs</h5>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                            
+                    </div>
+                    <div className="row justify-content-center">
+                    <div className="col-md-6 mt-4">
+                            <div className="card bg-dark text-white">
+                                <img className="card-img" style={styles} src="/static/images/front-page/fasces.jpg" alt="Card image" />
+                                <div className="card-img-overlay">
+                                    <h5 className="card-title">Write Blogs</h5>
+                                    
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -38,7 +79,7 @@ const Index = () => {
 
             </article>
 
-            <Link href="/signup"><a>Signup</a></Link>
+            
 
         </Layout>
     )
